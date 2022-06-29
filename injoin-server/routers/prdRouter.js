7d41ea6e8aa2ca528887c35b6dfb1dc4095eadb2
Ariel-mcu <judy3113461@gmail.com>
2022-06-29 16:35:32 +0800

    feat prdRouter.js pagination

--- injoin-server/routers/prdRouter.js
+++ injoin-server/routers/prdRouter.js
@@ -3,8 +3,34 @@ const router = express.Router();
 const pool = require('../utils/db');
 
 router.get('/', async (req, res, next) => {
-  let [data, fields] = await pool.execute('SELECT * FROM `prd_list`');
-  res.json(data);
+  // 當前頁面
+  let page = req.query.page || 1;
+
+  let [allData, fields] = await pool.execute('SELECT * FROM `prd_list` WHERE status = 1 AND category= ?', [req.query.category]);
+  
+  // 總數
+  const total = allData.length;
+
+  // 計算總頁數
+  const perPage = 12; // 每一頁有幾筆
+  const lastPage = Math.ceil(total / perPage);
+
+  // 計算要跳過幾筆）
+  let offset = (page - 1) * perPage;
+
+  // 取得這一頁的資料 select * from table limit ? offet ?
+  let [pageData] = await pool.execute(
+    'SELECT * FROM `prd_list` WHERE status = 1 AND category= ?  LIMIT ? OFFSET ?',
+    [req.query.category, perPage, offset]
+  );
+  res.json({
+    pagination: {
+      total,
+      lastPage,
+      page,
+    },
+    data: pageData,
+  });
 });
 
 module.exports = router;
