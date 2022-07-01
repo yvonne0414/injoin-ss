34fb26bac7a7f49b77a84a35099aaaf42ed576ba
jillmfee25 <lisa200325@gmail.com>
2022-07-01 20:39:02 +0800

    feat cartRouter

--- injoin-server/routers/cartRouter.js
+++ injoin-server/routers/cartRouter.js
@@ -6,11 +6,11 @@ const pool = require('../utils/db');
 const multer = require('multer');
 const path = require('path');
 
-// router.get('/', async (req, res, next) => {
-//   let [data, fields] = await pool.execute(
-//     'SELECT order_list.*, user_list.name as userName, logistics_state_cate.name as logiStaCateName, logistics_cate.name as logiCateName FROM order_list JOIN user_list ON order_list.user_id = user_list.id JOIN logistics_cate ON order_list.logistics = logistics_cate.id JOIN logistics_state_cate ON order_list.logistics_state = logistics_state_cate.id;'
-//   );
-//   res.json(data);
-// });
+/* GET products listing. */
+router.get('/', async (req, res, next) => {
+  console.log('hi~',req.body);
+  console.log(req.params);
+  console.log(req.query);
+});
 
 module.exports = router;
