c71d3fdb0242d81a597a10231ca720ea8f477f18
jillmfee25 <jillmfee25@users.noreply.github.com>
2022-07-02 09:46:21 +0800

    feat orderRouter

--- injoin-server/routers/orderRouter.js
+++ injoin-server/routers/orderRouter.js
@@ -14,7 +14,7 @@ router.use('/detail/:orderId', async (req, res, next) => {
   console.log(req.params);
   // console.log(req.query);
   let [orderUser] = await pool.execute(
-    `SELECT user_list.id, user_list.name, user_list.phone, user_list.email, user_list.address_country, user_list.address_detail, tw_county.name as countyName FROM user_list JOIN order_list ON user_list.id = order_list.user_id JOIN tw_county ON user_list.address_country = tw_county.code WHERE order_list.id = ?`,
+    `SELECT user_list.id, user_list.name, user_list.phone, user_list.email, user_list.address_country, user_list.address, tw_county.name as countyName FROM user_list JOIN order_list ON user_list.id = order_list.user_id JOIN tw_county ON user_list.address_country = tw_county.code WHERE order_list.id = ?`,
     [req.params.orderId]
   );
   // console.log(orderUser);
@@ -24,8 +24,6 @@ router.use('/detail/:orderId', async (req, res, next) => {
     [req.params.orderId]
   );
   console.log(orderDetailData);
-
-
 
   let [orderPrd] = await pool.execute(
     `SELECT prd_list.id,prd_list.prd_num,prd_list.name,prd_list.price,order_detail.amount,prd_list.category,prd_list.status,prd_list.main_img,prd_list.inventory_quantity FROM order_detail JOIN prd_list ON order_detail.prd_id = prd_list.id WHERE order_id = ?`,
@@ -41,23 +39,22 @@ router.use('/detail/:orderId', async (req, res, next) => {
   // {orderuser:orderUser, orederDetail:orderDetailData, orderprd:orderPrd}
 });
 
-
 // 訂單列表 + 分頁
 router.get('/', async (req, res, next) => {
   // 當前頁面
-  console.log(req.query);
+  // console.log(req.query);
   let page = req.query.page || 1;
   let userId = req.query.userId || 1;
   let logisticsState = req.query.logisticsState || 1;
 
   // 抓資料
   let [allData, fields] = await pool.execute(
-    `SELECT order_list.*, user_list.name as userName, user_list.phone as userPhone, user_list.email as userEmail, user_list.address_detail as userAddress, logistics_state_cate.name as logiStaCateName, logistics_cate.name as logiCateName FROM order_list JOIN user_list ON order_list.user_id = user_list.id JOIN logistics_cate ON order_list.logistics = logistics_cate.id JOIN logistics_state_cate ON order_list.logistics_state = logistics_state_cate.id WHERE order_list.user_id = ? AND order_list.logistics_state = ? `,
+    `SELECT order_list.*, user_list.name as userName, user_list.phone as userPhone, user_list.email as userEmail, user_list.address as userAddress, logistics_state_cate.name as logiStaCateName, logistics_cate.name as logiCateName FROM order_list JOIN user_list ON order_list.user_id = user_list.id JOIN logistics_cate ON order_list.logistics = logistics_cate.id JOIN logistics_state_cate ON order_list.logistics_state = logistics_state_cate.id WHERE order_list.user_id = ? AND order_list.logistics_state = ? `,
     [userId, logisticsState]
   );
   // 總數
   const total = allData.length;
-  console.log(total);
+  // console.log(total);
 
   // 計算總頁數
   const perPage = 5; // 每一頁有幾筆
@@ -68,7 +65,7 @@ router.get('/', async (req, res, next) => {
 
   // 取得這一頁的資料 select * from table limit ? offet ?
   let [pageData] = await pool.execute(
-    'SELECT order_list.*, user_list.name as userName, user_list.phone as userPhone, user_list.email as userEmail, user_list.address_detail as userAddress, logistics_state_cate.name as logiStaCateName, logistics_cate.name as logiCateName FROM order_list JOIN user_list ON order_list.user_id = user_list.id JOIN logistics_cate ON order_list.logistics = logistics_cate.id JOIN logistics_state_cate ON order_list.logistics_state = logistics_state_cate.id WHERE order_list.user_id = ? AND order_list.logistics_state = ? ORDER BY order_list.order_time DESC LIMIT ? OFFSET ?',
+    'SELECT order_list.*, user_list.name as userName, user_list.phone as userPhone, user_list.email as userEmail, user_list.address as userAddress, logistics_state_cate.name as logiStaCateName, logistics_cate.name as logiCateName FROM order_list JOIN user_list ON order_list.user_id = user_list.id JOIN logistics_cate ON order_list.logistics = logistics_cate.id JOIN logistics_state_cate ON order_list.logistics_state = logistics_state_cate.id WHERE order_list.user_id = ? AND order_list.logistics_state = ? ORDER BY order_list.order_time DESC LIMIT ? OFFSET ?',
     [userId, logisticsState, perPage, offset]
   );
 
